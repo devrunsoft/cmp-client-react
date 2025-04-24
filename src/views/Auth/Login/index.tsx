ff11f0fb6021bd247f655111b659a9fe1b8d6561
morteza <mortezasojodi70@gmail.com>
2025-04-24 19:48:33 +0330

    .

--- src/views/Auth/Login/index.tsx
+++ src/views/Auth/Login/index.tsx
@@ -169,7 +169,7 @@ const LoginForm = () => {
           fullWidth
           endIcon={<IoIosArrowRoundForward />}
         >
-          Continue
+          Login
         </ActiveLoadingButton>
       </Box>
     </Box>
