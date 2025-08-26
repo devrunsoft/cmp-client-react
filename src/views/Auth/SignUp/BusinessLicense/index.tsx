7f71a546715507d0b83626b82c0c3ec89d58e8ac
morteza <mortezasojodi70@gmail.com>
2025-08-27 00:59:29 +0330

    .

--- src/views/Auth/SignUp/BusinessLicense/index.tsx
+++ src/views/Auth/SignUp/BusinessLicense/index.tsx
@@ -64,7 +64,7 @@ export const BusinessLicense = ({
           toast.error(error.message);
         },
         (_) => {
-          setIndex!(4);
+          navigate("/", { replace: true });
         }
       );
     } finally {
