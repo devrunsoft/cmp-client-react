9fe2e41ef09a9535354cac5c1398f38d97f8be34
morteza <mortezasojodi70@gmail.com>
2025-09-07 15:23:25 +0330

    .

--- src/common/data/api/service_appointment/get_service_appointment_api.ts
+++ src/common/data/api/service_appointment/get_service_appointment_api.ts
@@ -1,21 +1,11 @@
+import { ClientBaseServiceEntity } from "cmp-core/src/entity/clientBaseService";
 import { String_Const } from "common/constants/string_constants";
 import { CusomerError, Either, left, right, UnAuthorize } from "common/core/either";
 import { BaseResponse } from "common/core/response/api_response";
-import { AddServiceAppointmentCommand } from "common/domain/command/service_appointment/add_service_appointment_command";
-import { SignCompanyContractCommand } from "common/domain/command/sign_contract_command";
-import { SignUpCommand } from "common/domain/command/signUpCommand";
-import { CapacityEntity } from "common/domain/entity/capacity_entity";
-import { ClientRepresentationEntity } from "common/domain/entity/client_representation_response";
-import { CompanyContractEntity } from "common/domain/entity/contract_entity";
-import { DocumentEntity } from "common/domain/entity/document_entity";
-import { InvoiceEntity } from "common/domain/entity/invoice_entity";
-import { OperationalAddressEntity } from "common/domain/entity/operational_address_entity";
-import { ServiceAppointmentEntity } from "common/domain/entity/service_appointment_entity";
-import { ServiceTypeEnum } from "common/domain/enum/service_type_enum";
 import { header, saveToken, tokenheader } from "core/src/utils/auth";
 import { Api_URL } from "core/src/utils/url";
 
-export async function getServiceAppointmentApi(Id: number): Promise<Either<Error, ServiceAppointmentEntity>> {
+export async function getServiceAppointmentApi(Id: number): Promise<Either<Error, ClientBaseServiceEntity>> {
     try {
         var h = header();
         const response = await fetch(`${Api_URL}/ServiceAppointment/${Id}`, {
@@ -23,7 +13,7 @@ export async function getServiceAppointmentApi(Id: number): Promise<Either<Error
             headers: h,
         });
         const data = await response.json();
-        const result: BaseResponse<ServiceAppointmentEntity> = data;
+        const result: BaseResponse<ClientBaseServiceEntity> = data;
 
         if (result.Success) {
             return right(result.Data);
